3b9c55dcc563b35073e8664c09fe68e939c21905
Gustavo Gomez <gustavo.gomezalv@uanl.edu.mx>
2025-10-04 14:27:57 -0600

    Upload Package Lock

--- server/index.js
+++ server/index.js
@@ -12,6 +12,10 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the NASA-Space App!');
+})
+
 //Run app
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
