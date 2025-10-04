10ad3515120834f097dbbd86e96695e9b85a0804
Gustavo Gomez <gustavo.gomezalv@uanl.edu.mx>
2025-10-04 13:55:47 -0600

    Init app

---
+++ server/index.js
@@ -0,0 +1,18 @@
+//Import Modules and Router
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+//Basic Config
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+//Settings and Routes of app
+app.use(cors());
+app.use(express.json());
+
+//Run app
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
