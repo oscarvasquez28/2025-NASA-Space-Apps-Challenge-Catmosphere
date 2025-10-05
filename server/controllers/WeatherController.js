99641453d3bf20981c802e71d7cbf0aef4efd41f
Gustavo Gomez <gustavo.gomezalv@uanl.edu.mx>
2025-10-04 21:18:59 -0600

    Get Weather

---
+++ server/controllers/WeatherController.js
@@ -0,0 +1,40 @@
+export const getWeather = async (req, res) => {
+    try {
+        const errors = {}
+        const {
+            latitude,
+            longitude
+        } = req.params;
+        const apiKey = process.env.OPEN_WEATHER_KEY
+
+        if (Object.keys(errors) <= 0) {
+            const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly&units=metric&appid=${apiKey}`);
+
+            if (!response.ok) {
+                return res.status(400).json({
+                    errors: errors,
+                    msg: "Ocurrio un error al obtener el clima",
+                    success: false
+                })
+
+            }
+
+            const data = await response.json();
+
+            const payload = {
+                temperature: data?.current?.temp,
+                summary: data?.current?.weather[0].description,
+            }
+
+            return res.status(200).json({
+                data: data,
+                msg: "Se obtuvo la temperatura correctamente",
+                success: true,
+            })
+        }
+
+        return res.status(400).json({errors: errors, msg: "Ocurrio un error al obtener el clima", success: false})
+    } catch (error) {
+        res.status(500).json({success: false, msg: error, errors: []})
+    }
+};
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import NotificationRouter from './routes/NotificationRouter.js';
 import UserRouter from "./routes/UserRouter.js";
+import WeatherRouter from "./routes/WeatherRouter.js";
 
 //Basic Config
 const app = express();
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/notifications", NotificationRouter)
 app.use("/api/users", UserRouter)
+app.use("/api/weather", WeatherRouter)
 
 //Run app
 app.listen(port, () => {
