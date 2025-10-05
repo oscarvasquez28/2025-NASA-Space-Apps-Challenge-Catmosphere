0c27e1c85094ad4522bd5b4df8141af2c8b4f623
Gustavo Gomez <gustavo.gomezalv@uanl.edu.mx>
2025-10-04 18:39:07 -0600

    DB_Hosting

--- server/config/database.js
+++ server/config/database.js
@@ -1,9 +1,17 @@
 import {Sequelize} from 'sequelize';
+import * as path from "node:path";
+import * as fs from "node:fs";
 
 const dbConnection = new Sequelize(process.env.DB_NAME, process.env.USER_NAME, process.env.USER_PASSWORD, {
-    host: "localhost",
+    host: process.env.DB_HOST,
     dialect: "mysql",
-    port: 3306,
+    port: process.env.DB_PORT,
+    dialectOptions: {
+        ssl: {
+            ca: fs.readFileSync(path.resolve('./isrgrootx1.pem')).toString(),
+            rejectUnauthorized: true
+        }
+    },
 });
 
 (async () => {
